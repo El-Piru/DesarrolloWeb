315932f4a1d6fa1f8270f7015cb2038807163c25
El-Piru <dolly22353@gmail.com>
2026-09-13 11:49:36 -0300

    Se agrego opciones en el script.js para la opcion 'En Vivo'

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-const boton = document.querySelector('#BOTTOMSINGUP');
-const estado = document.querySelector('.estado');
-const reloj = document.querySelector('#reloj');
-const inputCliente = document.getElementById('inputCliente');
-let estadoActual = false;
-let segundos =0;
-let cronometro;
+const botonesNav = document.querySelectorAll('.nav-link');
+const vistas = document.querySelectorAll('.vista-content');
 
-boton.addEventListener('click', function() {
-    if(estadoActual) {
-        clearInterval(cronometro);
-        let cobro = segundos * 0.5;
-        alert('Sesion terminada ' + inputCliente.value + '. Tiempo: '+ segundos + 's. Total a pagar: $' + cobro);
-        reloj.innerHTML = 'Tiempo : 00:00:00';
-        segundos = 0;
-    estado.innerText = 'UwU';
-    inputCliente.value = '';
-    inputCliente.disabled = false;
-    estado.style.color = '#10b981';
-    boton.innerText = 'Iniciar Sesion';
-    estadoActual = false;
-    }else{
-    inputCliente.disabled = true;
-    cronometro = setInterval(function() {
-        segundos++;
-        reloj.innerHTML = 'Tiempo : ' + segundos + 's';
-    }, 1000);
-    estado.innerText = 'OwO';
-    estado.style.color = '#d85407';
-    boton.innerText = 'Cerrar Sesion';
-    estadoActual = true;
-    }
+botonesNav.forEach(function(boton) {
+    boton.addEventListener('click', function() {
+
+        botonesNav.forEach(function(b){b.classList.remove('active'); });
+        vistas.forEach(function(v){v.classList.remove('active'); });
+        boton.classList.add('active');
+
+        if(boton.innerText === 'Crear Partido'){
+            doacument.getElementById('VistaNuevoPartido').classlist.add('active');
+        }else if(boton.innerText === 'En Vivo'){
+            document.getElementById('VistaEnVivo').classList.add('active');
+
+        }
+    });
 });
