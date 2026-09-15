46f35413fd2e4d8686b1ace74d84eb6d70f0006f
leidy <leidyyt03@gmail.com>
2026-09-15 15:27:26 +0000

    agregue confi

--- script.js
+++ script.js
@@ -25,6 +25,10 @@ document.getElementById('btnHubPartidos').addEventListener('click', function() {
     mostrarVista('VistaPartidos');
 });
 
+document.getElementById('btnHubConfig')?.addEventListener('click', function() {
+    mostrarVista('VistaConfig');
+});
+
 const btnEquipos = document.getElementById('btnHubEquipos');
 if (btnEquipos) {
     btnEquipos.addEventListener('click', function() {
@@ -352,3 +356,18 @@ if(partidoFinalizado){
         //renderizarVistaPartidos();
         mostrarVista('VistaInicio');
 })}
+
+const btnGuardarConfig = document.getElementById('btnGuardarConfig');
+
+if (btnGuardarConfig) {
+    btnGuardarConfig.addEventListener('click', function() {
+
+        const nombre = document.getElementById('nombreArbitro').value;
+
+        document.getElementById('userBadge').innerText =
+            `Árbitro: ${nombre}`;
+
+        alert('Configuración guardada correctamente.');
+    });
+}
+ 
